c11ef8e461af74825e2effc5a2e1b8ce88570f9b
Adrian Lee <adrianlee0118@gmail.com>
2020-07-06 14:13:21 -0700

    added conditional renders for handling loading and errors

--- src/App.js
+++ src/App.js
@@ -1,33 +1,68 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, Fragment } from "react";
 import axios from "axios";
+import ContentLoader from "react-content-loader";
 
 const App = () => (
   <div>
-    <APIList />
+    <APISearchList />
   </div>
 );
 
-const APIList = () => {
+const APISearchList = () => {
   const [data, setData] = useState({ hits: [] });
+  const [query, setQuery] = useState("redux"); //value of search field
+  const [url, setUrl] = useState(
+    //url obtained from query value in search field
+    "https://hn.algolia.com/api/v1/search?query=redux"
+  );
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
 
   useEffect(() => {
     const fetchData = async () => {
-      const result = await axios(
-        "https://hn.algolia.com/api/v1/search?query=redux"
-      );
-      setData(result.data);
+      setIsError(false);
+      setIsLoading(true);
+
+      try {
+        const result = await axios(url);
+        setData(result.data);
+      } catch (error) {
+        setIsError(true);
+      }
+
+      setIsLoading(false);
     };
     fetchData();
-  }, []);
+  }, [url]); //re-render occurs only when url changes
 
   return (
-    <ul>
-      {data.hits.map((item) => (
-        <li key={item.objectID}>
-          <a href={item.url}>{item.title}</a>
-        </li>
-      ))}
-    </ul>
+    <Fragment>
+      <input
+        type="text"
+        value={query}
+        onChange={(event) => setQuery(event.target.value)}
+      />
+      <button
+        type="button"
+        onClick={() =>
+          setUrl(`https://hn.algolia.com/api/v1/search?query=${query}`)
+        }
+      >
+        Search
+      </button>
+      {isError && <div>Something went wrong...</div>}
+      {isLoading ? (
+        <ContentLoader />
+      ) : (
+        <ul>
+          {data.hits.map((item) => (
+            <li key={item.objectID}>
+              <a href={item.url}>{item.title}</a>
+            </li>
+          ))}
+        </ul>
+      )}
+    </Fragment>
   );
 };
 
