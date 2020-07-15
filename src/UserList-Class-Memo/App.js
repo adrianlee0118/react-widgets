3864c78d286ccc11d8749b55120c0fb1ee09a950
Adrian Lee <adrianlee0118@gmail.com>
2020-07-15 14:25:24 -0700

    added class

--- src/UserList-Class-Memo/App.js
+++ src/UserList-Class-Memo/App.js
@@ -1,6 +1,6 @@
 //NEXT: tictactoe mods, then userReducer with all FcnComps then Invoice Editor w/o material-ui : class, fcncomp, usereducer hooks, react-redux
 //Using React's MEMO API to optimize render by preventing default unnecessary re-renders of components that haven't changed
-import React, { memo, useCallback } from "react";
+import React, { memo } from "react";
 import { v4 as uuidv4 } from "uuid";
 
 class UserList extends React.Component {
