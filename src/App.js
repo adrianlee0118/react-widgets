192e6e6c541e6dbba7a730cd299bcc78f4be0b47
Adrian Lee <adrianlee0118@gmail.com>
2020-07-16 11:12:34 -0700

    added FcnComp

--- src/App.js
+++ src/App.js
@@ -1,106 +1,57 @@
-//NEXT: tictactoe mods, then userReducer with all FcnComps then Invoice Editor w/o material-ui : class, fcncomp, usereducer hooks, react-redux
-//Using React's MEMO API to optimize render by preventing default unnecessary re-renders of components that haven't changed
-import React, { memo, useCallback } from "react";
-import { v4 as uuidv4 } from "uuid";
-
-class UserList extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      users: [
-        { id: "a", name: "Robin" },
-        { id: "b", name: "Dennis" },
-      ],
-      text: "",
-    };
-  }
-  handleText = (event) => {
-    this.setState({ text: event.target.value });
-  };
-  handleAddUser = () => {
-    this.setState({
-      users: [...this.state.users, { id: uuidv4(), name: this.state.text }],
-    });
-  };
-  handleRemove = (id) => {
-    this.setState({
-      users: this.state.users.filter((user) => user.id !== id),
-    });
-  };
-  render() {
-    return (
-      <div>
-        <input type="text" value={this.state.text} onChange={this.handleText} />
-        <button type="button" onClick={this.handleAddUser}>
-          Add User
-        </button>
-        <List list={this.state.users} onRemove={this.handleRemove} />
-      </div>
-    );
-  }
-}
-
-/* Function component with hooks replaced by class above
-const UserList = () => {
-  console.log("Render: App");
-  const [users, setUsers] = useState([
-    { id: "a", name: "Robin" },
-    { id: "b", name: "Dennis" },
-  ]);
-  const [text, setText] = useState("");
-  const handleText = (event) => {
-    setText(event.target.value);
-  };
-  const handleAddUser = () => {
-    setUsers(users.concat({ id: uuidv4(), name: text }));
-  };
-  //useCallback to memoize a function so that it only gets re-defined when dependencies ([users]) change rather than on every change to input
-  const handleRemove = useCallback(
-    (id) => (setUsers(users.filter((user) => user.id !== id)), [users])
-  );
-  return (
-    <div>
-      <input type="text" value={text} onChange={handleText} />
-      <button type="button" onClick={handleAddUser}>
-        Add User
-      </button>
-      <List list={users} onRemove={handleRemove} />
-    </div>
-  );
-};
-*/
-
-//React memo prevents List from re-rendering when the input field's contents are changed
-//Only UserList will re-render, not the List component
-const List = memo(({ list, onRemove }) => {
-  console.log("Render: List");
-  return (
-    <ul>
-      {list.map((item) => (
-        <ListItem key={item.id} item={item} onRemove={onRemove} />
-      ))}
-    </ul>
-  );
-});
-
-//React memo again prevents ListItems from re-rendering if unnecessary by i.e. simply changing content of input field
-//When characters are added to input field, only the App component re-renders, not List and ListItems
-const ListItem = memo(({ item, onRemove }) => {
-  console.log("Render: ListItem");
-  return (
-    <li>
-      {item.name}
-      <button type="button" onClick={() => onRemove(item.id)}>
-        Remove
-      </button>
-    </li>
-  );
-});
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 const App = () => (
   <div>
-    <UserList />
+    <Clock />
   </div>
 );
+
+const Clock = () => {
+  const [time, setTime] = useState(new Date());
+  useEffect(() => {
+    const interval = setInterval(() => setTime(new Date()), 1000);
+    return () => clearInterval(interval);
+  }, [time]);
+  return (
+    <div>
+      <h1>Hello world!</h1>
+      <h2>It is {time.toLocaleTimeString()}.</h2>
+    </div>
+  );
+};
+
+/*
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      date: new Date(),
+    };
+  }
+
+  componentDidMount() {
+    this.timerID = setInterval(() => this.tick(), 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState({
+      date: new Date(),
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hello, world!</h1>
+        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+      </div>
+    );
+  }
+}*/
 
 export default App;
--- src/Clock-FcnComp/App.js
+++ src/Clock-FcnComp/App.js
@@ -7,21 +7,51 @@ const App = () => (
   </div>
 );
 
-//Notes:
-//Use React hooks useState to simulate state, and useEffect to simulate didMount lifecycle methods in a function component
 const Clock = () => {
   const [time, setTime] = useState(new Date());
   useEffect(() => {
     const interval = setInterval(() => setTime(new Date()), 1000);
-    return () => clearInterval(interval); //cleanup function
+    return () => clearInterval(interval);
   }, [time]);
-
   return (
     <div>
-      <h1>Hello, world!</h1>
+      <h1>Hello world!</h1>
       <h2>It is {time.toLocaleTimeString()}.</h2>
     </div>
   );
 };
+
+/*
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      date: new Date(),
+    };
+  }
+
+  componentDidMount() {
+    this.timerID = setInterval(() => this.tick(), 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState({
+      date: new Date(),
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hello, world!</h1>
+        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+      </div>
+    );
+  }
+}*/
 
 export default App;
