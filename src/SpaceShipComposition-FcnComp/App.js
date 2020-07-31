44a326a1768acd9d3abcbe195b4a93ee6242d2a7
Adrian Lee <adrianlee0118@gmail.com>
2020-07-31 13:40:32 -0700

    added function component

--- src/SpaceShipComposition-FcnComp/App.js
+++ src/SpaceShipComposition-FcnComp/App.js
@@ -53,32 +53,4 @@ const SignUpDialog = () => {
   );
 };
 
-/*
-class SignUpDialog extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSignUp = this.handleSignUp.bind(this);
-    this.state = { login: "" };
-  }
-  handleChange(event) {
-    this.setState({ login: event.target.value });
-  }
-  handleSignUp() {
-    alert(`Welcome aboard, ${this.state.login}!`);
-  }
-  render() {
-    return (
-      <Dialog
-        title="Mars Exploration Program"
-        message="How should we refer to you?"
-      >
-        <input value={this.state.login} onChange={this.handleChange} />
-        <button onClick={this.handleSignUp}>Sign Me Up!</button>
-      </Dialog>
-    );
-  }
-}
-*/
-
 export default App;
