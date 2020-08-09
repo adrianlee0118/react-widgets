a7eb309a508f2cf3a0d8303de71420854efefe66
Adrian Lee <adrianlee0118@gmail.com>
2020-08-09 16:43:29 -0700

    added netflix slider example widget

--- src/App.js
+++ src/App.js
@@ -1,15 +1,53 @@
 import React from "react";
+import cx from "classnames";
+
+import SliderContext from "";
+import Content from "";
+import SlideButton from "";
+import SliderWrapper from "";
+import useSliding from "";
+import useSizeElement from "";
+
 import "./App.css";
 
-const NetflixSlider = () => (
-  <div class="container">
-    <div class="item">1</div>
-    <div class="item">2</div>
-    <div class="item">3</div>
-    <div class="item">4</div>
-    <div class="item">4</div>
-  </div>
-);
+const NetflixSlider = () => {
+  const [currentSlide, setCurrentSlide] = useState(activeSlide);
+  const { width, elementRef } = useSizeElement();
+  const {
+    handlePrev,
+    handleNext,
+    slideProps,
+    containerRef,
+    hasNext,
+    hasPrev,
+  } = useSliding(width, React.Children.count(children));
+
+  const handleSelect = (movie) => {
+    setCurrentSlide(movie);
+  };
+
+  const contextValue = {
+    onSelectSlide: handleSelect,
+    onCloseSlide: handleClose,
+    elementRef,
+    currentSlide,
+  };
+
+  return (
+    <SliderContext.Provider value={contextValue}>
+      <SliderWrapper>
+        <div className={cx("slider", { "slider--open": currentSlide != null })}>
+          <div ref={containerRef} className="slider__container" {...slideProps}>
+            {children}
+          </div>
+        </div>
+        {hasPrev && <SlideButton onClick={handlePrev} type="prev" />}
+        {hasNext && <SlideButton onClick={handleNext} type="next" />}
+      </SliderWrapper>
+      {currentSlide && <Content movie={currentSlide} onClose={handleClose} />}
+    </SliderContext.Provider>
+  );
+};
 
 const App = () => <NetflixSlider />;
 
